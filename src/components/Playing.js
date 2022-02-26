30bda5d81a06d5c3580f669343ca5d5fb710c477
tanhoang0710 <tanhoang0710@gmail.com>
2022-02-26 11:26:56 +0700

    first upload

---
+++ src/components/Playing.js
@@ -0,0 +1,31 @@
+import React from "react";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import { Songs } from "../Context";
+import { useContext } from "react";
+
+export default function Playing() {
+	const { song, handleSetSong } = useContext(Songs);
+
+	const handleClickNext = () => {
+		handleSetSong(song.id + 1);
+	};
+
+	const handleClickPrev = () => {
+		handleSetSong(song.id - 1);
+	};
+
+	return (
+		<div>
+			<AudioPlayer
+				className="player-music"
+				src={song.url}
+				layout="stacked-reverse"
+				showSkipControls={true}
+				showJumpControls={false}
+				onClickNext={handleClickNext}
+				onClickPrev={handleClickPrev}
+			/>
+		</div>
+	);
+}
