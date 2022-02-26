0e389e2bea6b56689ecd90a2ffbeebdcf1f7e74f
tanhoang0710 <tanhoang0710@gmail.com>
2022-02-26 12:23:24 +0700

    add text

--- src/components/DetailSong.js
+++ src/components/DetailSong.js
@@ -7,7 +7,7 @@ export default function DetailSong() {
 	const { song } = useContext(Songs);
 
 	return (
-		<div className="col-span-1 p-3">
+		<div className="col-span-1 p-3 flex flex-col align-center">
 			<h2 className="text-cyan-500 font-bold">Now playing</h2>
 			<h2 className="text-neutral-400 text-2xl ">{song.name}</h2>
 			<div className="w-[240px] m-auto mt-10">
@@ -25,6 +25,9 @@ export default function DetailSong() {
 				/>
 				<span className="text-xl text-white">{song.author}</span>
 			</div>
+			<span className="mt-auto text-white text-center">
+				Made with <i class="fa-solid fa-heart text-red-500 "></i>
+			</span>
 		</div>
 	);
 }
