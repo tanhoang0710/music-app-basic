c42497aa00ff97ea129ba781925b67181845fb92
tanhoang0710 <tanhoang0710@gmail.com>
2022-02-27 09:49:44 +0700

    change title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,9 @@ import React from "react";
 export default function Navbar() {
 	return (
 		<div className="h-24 bg-slate-900 text-white text-center leading-[6rem] text-3xl">
-			<i className="fa-brands fa-spotify mr-5"></i>
-			Spotify
+			{/* <i className="fa-brands fa-spotify mr-5"></i>
+			Spotify */}
+			Bruhhhhhhhhhhhhhhhhhhhhhh......
 		</div>
 	);
 }
